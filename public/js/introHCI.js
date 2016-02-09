0f702f20d391fed3ebf3d9d434d3c23934e32560
Janarthan Nadendla <janarthan.nadendla@gmail.com>
2016-02-09 05:23:12 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -25,7 +25,15 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
+	$.get("/project/" + idNumber, function(result){
+		console.log(result);
+		var title = "<h1>" + result.title + " " + result.date + "</h1>";
+		var summary = "<p>" + result.summary + "</p>";
+		var img = "<img src=" + result.image + ">"; 
 
+		//insert to div
+		$("#" + projectID + " .details").html(title + summary + img);
+	});
 	console.log("User clicked on project " + idNumber);
 }
 
@@ -35,4 +43,15 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	$.get("/palette", function(result){
+		console.log(result.colors.hex);
+		var colors = result.colors.hex;
+
+		$('body').css('background-color', colors[0]);
+		$('.thumbnail').css('background-color', colors[1]);
+		$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+		$('p').css('color', colors[3]);
+		$('.project img').css('opacity', .75);
+	});
 }
